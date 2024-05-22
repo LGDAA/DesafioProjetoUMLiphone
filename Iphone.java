f8bde57fc3f1bfb07dbbb4df145b0bb2f341f236
Luiz Gustavo <165921842+LGDAA@users.noreply.github.com>
2024-05-22 18:58:49 -0300

    Create Iphone.java

---
+++ Iphone.java
@@ -0,0 +1,10 @@
+package UMLiphone;
+
+public class Iphone {
+    public static void main(String[] args) {
+        Funcionalidades iphone = new Funcionalidades();
+        iphone.ligar("8199230540");
+        iphone.atender();
+    }
+}
+ 
