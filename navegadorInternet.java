dc88821fa6ed9450ea7281d07397c6b5cd848953
Luiz Gustavo <165921842+LGDAA@users.noreply.github.com>
2024-05-22 18:59:08 -0300

    Create navegadorInternet.java

---
+++ navegadorInternet.java
@@ -0,0 +1,7 @@
+package UMLiphone;
+
+public interface navegadorInternet {
+    public void exibirPagina(String urlPagina);
+    public void adicionarNovaAba();
+    public void atualizarPagina();
+}
